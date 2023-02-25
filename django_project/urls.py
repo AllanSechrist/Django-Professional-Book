27ed0d0787c6b1c1c6a6f98b94ee669be9e04f4b
AllanSechrist <sechristallan@gmail.com>
2023-02-25 17:23:43 +0900

    ch6 add login, logout, signup pages

--- django_project/urls.py
+++ django_project/urls.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    #Admin
     path("admin/", admin.site.urls),
+    #Users
+    path("accounts/", include("django.contrib.auth.urls")),
+    #Local
+    path("accounts/", include("accounts.urls")),
     path("", include("pages.urls")),
 ]
